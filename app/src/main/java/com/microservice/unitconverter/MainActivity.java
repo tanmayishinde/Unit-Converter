84edf92db87e92531398ef05d5541e994b21ca3b
tanmayishinde <tanmayi.shinde27@gmail.com>
2022-10-06 08:35:46 +0530

    Second Commit

--- app/src/main/java/com/microservice/unitconverter/MainActivity.java
+++ app/src/main/java/com/microservice/unitconverter/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ *
+ *   Created Tanmayi Shinde on 06-10-2022  08:28 AM
+ *   Copyright Ⓒ 2022. All rights reserved Ⓒ 2022 http://freefuninfo.com/
+ *   Last modified: 06-10-2022  08:28 AM
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENS... Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *    either express or implied. See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * /
+ */
+
 package com.microservice.unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,20 +42,37 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView2);
         editText = findViewById(R.id.editText);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //if  add Tost
-                //Toast.makeText(MainActivity.this, "Hi Tanmayi here!!!", Toast.LENGTH_SHORT).show();
-                //Get Input in String format
+    //2 ways  of setting Button Function:
+    //1. By Event Listner
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //if  add Tost
+//                //Toast.makeText(MainActivity.this, "Hi Tanmayi here!!!", Toast.LENGTH_SHORT).show();
+//                //Get Input in String format
+//                String getInputInString = editText.getText().toString();
+//                //Save it in Integer format
+//               double kgIsconvrtInputToDouble = Double.parseDouble(getInputInString);
+//
+//               double pound = kgIsconvrtInputToDouble * 2.205;
+//
+//               textView.setText("The corresponding value in pound is " +pound);
+//            }
+//        });
+
+    }
+    //2. Using Method to set Button Function:
+    // Need to also declare this method in xml: android:onClick="onCalculate"
+        public void onCalculate(View view){
+            //Get Input in String format
                 String getInputInString = editText.getText().toString();
                 //Save it in Integer format
                double kgIsconvrtInputToDouble = Double.parseDouble(getInputInString);
-
+                //Calculate
                double pound = kgIsconvrtInputToDouble * 2.205;
-
+                //Display
                textView.setText("The corresponding value in pound is " +pound);
-            }
-        });
-    }
+        }
+
+
 }
